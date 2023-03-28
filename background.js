93027f745fa55ebde9c3f2931ace428379565373
Leandro Martelli <martelli@epix.com.br>
2023-03-28 09:34:56 +0200

    Block regardless of the host name
    /zephr/future-decisions is called from different host names, so care about the path only

--- background.js
+++ background.js
@@ -3,6 +3,6 @@ chrome.webRequest.onBeforeRequest.addListener(
     console.log("Blocked: "+details.url);
     return {cancel: true};
   },
-   {urls: ["https://static.infoglobo.com.br/paywall/*", "https://s1.wp.com/wp-content/themes/vip/abril-plugins/abril-piano/js/piano.js*", "https://paywall.folha.uol.com.br/*", "https://acesso.estadao.com.br/paywall/pw.js", "https://www.estadao.com.br/zephr/feature-decisions"]},
+   {urls: ["https://static.infoglobo.com.br/paywall/*", "https://s1.wp.com/wp-content/themes/vip/abril-plugins/abril-piano/js/piano.js*", "https://paywall.folha.uol.com.br/*", "https://acesso.estadao.com.br/paywall/pw.js", "https://*/zephr/feature-decisions"]},
   ["blocking"]
 );
